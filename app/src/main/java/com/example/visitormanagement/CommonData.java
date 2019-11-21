c0d2ccbc12ddcf5b65329cb9c438e72f13adf9f0
Manish7770 <17ucs087@lnmiit.ac.in>
2019-11-21 19:49:19 +0530

    Visitor CheckIn Application - Add getDate() and getTime() functions for conversion from Timestamp.

--- app/src/main/java/com/example/visitormanagement/CommonData.java
+++ app/src/main/java/com/example/visitormanagement/CommonData.java
@@ -1,10 +1,31 @@
 package com.example.visitormanagement;
 
+import android.text.format.DateFormat;
+
 import com.example.visitormanagement.Model.HostModel;
 import com.example.visitormanagement.Model.VisitorModel;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CommonData {
 
     public static HostModel selectedhost = new HostModel();
     public static VisitorModel visitordetails = new VisitorModel();
+
+    public static String getDate(long time)
+    {
+        Calendar calendar=Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(time);
+        StringBuilder date=new StringBuilder(DateFormat.format("dd-MM-yyyy",calendar).toString());
+        return date.toString();
+    }
+    public static String getTime(long time)
+    {
+        Calendar calendar=Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(time);
+        StringBuilder time2=new StringBuilder(DateFormat.format("HH:mm",calendar).toString());
+        return time2.toString();
+    }
+
 }
